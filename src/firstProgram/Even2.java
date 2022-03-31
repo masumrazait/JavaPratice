dd192ade896bcced0ff3fd3989dcb9e3e094f938
Gowthami <gowthami0622@gmail.com>
2022-03-31 23:02:10 +0530

    updated even program

---
+++ src/firstProgram/Even2.java
@@ -0,0 +1,19 @@
+package firstProgram;
+
+import java.util.Scanner;
+
+public class Even2 {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
+}
